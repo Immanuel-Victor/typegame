275330ac5bb7287bd2eb46b90c0e9de8bced7d42
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2022-10-28 11:38:10 -0300

    feat: adds dinamic position typing and initial entity logic

---
+++ src/entities/entity.ts
@@ -0,0 +1,36 @@
+import { Vector } from "../types/lib";
+
+export default class Entity {
+  protected position: Vector;
+  protected color: string;
+  protected size: Vector;
+
+  constructor(position: Vector, size: Vector, color: string) {
+    this.position = position;
+    this.color = color;
+    this.size = size;
+  }
+
+  public draw(context: CanvasRenderingContext2D, cam: Vector): void {
+    context.fillStyle = this.color;
+    context.fillRect(
+      this.position.x - this.size.x / 2 - cam.x,
+      this.position.y - this.size.y / 2 - cam.y,
+      this.size.x,
+      this.size.y
+    );
+  }
+
+  public get getPosition(): Vector {
+    return this.position;
+  }
+
+  public get getBoundries() {
+    return {
+      left: this.position.x - this.size.x / 2,
+      right: this.position.x + this.size.x / 2,
+      top: this.position.y - this.size.y / 2,
+      down: this.position.y + this.size.y / 2,
+    };
+  }
+}
